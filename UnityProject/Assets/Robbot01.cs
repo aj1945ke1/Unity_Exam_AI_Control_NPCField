3462064e37039835666120747bb5394cc64162ca
aj1945ke1 <57281981+aj1945ke1@users.noreply.github.com>
2019-11-07 10:49:10 +0800

    使用r3d.AddForce製作

---
+++ UnityProject/Assets/Robbot01.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class robotball : MonoBehaviour
+{
+    [Header("對話")]
+    public string Say = "碧蘭航線無限期徵指揮官！！！";
+    public float SaySpeed = 0.2f;
+    [Header("任務")]
+    public bool Complete = false;
+    public int BateryFinish = 5;
+    public int Batery = 0;
+}
+
